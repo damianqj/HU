f5e508ee7df3befd5e892bbf8790ca0208e0553a
Damian <damian.jorritsma@student.hu.nl>
2018-02-26 14:10:19 +0100

    test

---
+++ Les2/pe2_2.py
@@ -0,0 +1,13 @@
+cijferDH = 8
+cijferPG = 6
+cijferPJ = 7
+som = cijferDH + cijferPG + cijferPJ
+gemiddelde = som/3
+beloning = som * 30
+overzicht = 'Mijn cijfers (gemiddeld een ' +str(gemiddelde) + ') leveren een beloning van € ' +str(beloning)+ ' op!'
+print(overzicht)
+
+
+
+
+
