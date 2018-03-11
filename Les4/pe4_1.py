b6d5b951bdc670d20808649b3059a9325a56736e
Damian <damian.jorritsma@student.hu.nl>
2018-03-11 17:33:09 +0100

    test

---
+++ Les4/pe4_1.py
@@ -0,0 +1,5 @@
+def som(num1,num2,num3):
+    som = num1 + num2 + num3
+    return som
+
+print(som(1000, 27, 33))
---
+++ Les4/pe4_2.py
@@ -0,0 +1,6 @@
+def som(getallenlijst):
+    y = 0
+    for x in getallenlijst:
+        y += x
+    return y
+print(som((77, 144, 3, 99, 7)))
