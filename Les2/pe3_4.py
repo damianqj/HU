a6ae31b0247cbaa2e2f4bcd3c2180f200d543b0c
Damian <damian.jorritsma@student.hu.nl>
2018-03-11 15:55:45 +0100

    test

---
+++ Les2/pe3_4.py
@@ -0,0 +1,12 @@
+week=['maandag','dinsdag','woensdag']
+ma = 'ma'
+di = 'di'
+woe = 'woe'
+
+for woord in week:
+    if 'ma' in woord:
+        print(ma)
+    if 'di' in woord:
+        print(di)
+    if 'woe' in woord:
+        print(woe)
---
+++ Les2/pe3_6.py
@@ -0,0 +1,5 @@
+s = 'Guido van Rossum heeft programmeertaal python bedacht.'
+
+for char in s:
+    if char in 'aeoiuAEIOU':
+        print(char)
